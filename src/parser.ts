00eca5b899c821d978d2db9c70e542e1535dead1
Matt Newton <newton10471@gmail.com>
2025-03-24 18:51:05 -0400

    use spaces instead of tabs

--- src/parser.ts
+++ src/parser.ts
@@ -1,53 +1,53 @@
 function parseRESP(input: string): any {
-	switch (input[0]) {
-		case '+': // simple string
-			return parseSimpleString(input);
-		case '-': // error
-			return parseError(input);
-		case ':': // integer
-			return parseInteger(input);
-		case '$': // bulk string
-			return parseBulkString(input);
-		case '*': // array
-			return parseArray(input);
-		default:
-			return null;
-	}
+  switch (input[0]) {
+    case '+': // simple string
+      return parseSimpleString(input);
+    case '-': // error
+      return parseError(input);
+    case ':': // integer
+      return parseInteger(input);
+    case '$': // bulk string
+      return parseBulkString(input);
+    case '*': // array
+      return parseArray(input);
+    default:
+      return null;
+  }
 }
 
 function parseSimpleString(input: string): string {
-	return input.slice(1, -2);
+  return input.slice(1, -2);
 }
 
 function parseError(input: string): Error {
-	return new Error(input.slice(1, -2));
+  return new Error(input.slice(1, -2));
 }
 
 function parseInteger(input: string): number {
-	return Number(input.slice(1, -2));
+  return Number(input.slice(1, -2));
 }
 
 function parseBulkString(input: string): string | null {
-	const length = Number(input.slice(1, input.indexOf('\r\n')));
-	if (length === -1) {
-		return null;
-	}
-	const start = input.indexOf('\r\n') + 2;
-	return input.slice(start, start + length);
+  const length = Number(input.slice(1, input.indexOf('\r\n')));
+  if (length === -1) {
+    return null;
+  }
+  const start = input.indexOf('\r\n') + 2;
+  return input.slice(start, start + length);
 }
 
 function parseArray(input: string): any[] {
-	const length = Number(input.slice(1, input.indexOf('\r\n')));
-	let start = input.indexOf('\r\n') + 2;
-	const result = [];
-	let i = 0;
-	while (i < length) {
-		const item = parseRESP(input.slice(start));
-		result.push(item);
-		start += input.indexOf('\r\n') + 2;
-		i++;
-	}
-	return result;
+  const length = Number(input.slice(1, input.indexOf('\r\n')));
+  let start = input.indexOf('\r\n') + 2;
+  const result = [];
+  let i = 0;
+  while (i < length) {
+    const item = parseRESP(input.slice(start));
+    result.push(item);
+    start += input.indexOf('\r\n') + 2;
+    i++;
+  }
+  return result;
 }
 
 export { parseRESP };
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -2,23 +2,23 @@ import { describe, it, expect } from 'vitest';
 import { parseRESP } from '../src/parser';
 
 describe('RESP Parser', () => {
-	it('parses null bulk string', () => {
-		expect(parseRESP('$-1\r\n')).toEqual(null);
-	});
+  it('parses null bulk string', () => {
+    expect(parseRESP('$-1\r\n')).toEqual(null);
+  });
 
-	it('parses simple array with one bulk string', () => {
-		expect(parseRESP('*1\r\n$4\r\nping\r\n')).toEqual(['ping']);
-	});
+  it('parses simple array with one bulk string', () => {
+    expect(parseRESP('*1\r\n$4\r\nping\r\n')).toEqual(['ping']);
+  });
 
-	it('parses ECHO command', () => {
-		expect(parseRESP('*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n')).toEqual(['echo', 'hello world']);
-	});
+  it('parses ECHO command', () => {
+    expect(parseRESP('*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n')).toEqual(['echo', 'hello world']);
+  });
 
-	it('parses GET command', () => {
-		expect(parseRESP('*2\r\n$3\r\nget\r\n$3\r\nkey\r\n')).toEqual(['get', 'key']);
-	});
+  it('parses GET command', () => {
+    expect(parseRESP('*2\r\n$3\r\nget\r\n$3\r\nkey\r\n')).toEqual(['get', 'key']);
+  });
 
-	it('throws on malformed input', () => {
-		expect(() => parseRESP('*2\r\n$3\r\nbad')).toThrow();
-	});
+  it('throws on malformed input', () => {
+    expect(() => parseRESP('*2\r\n$3\r\nbad')).toThrow();
+  });
 });
