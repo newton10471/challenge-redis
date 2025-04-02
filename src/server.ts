50bdd04c982fe6b953f883e8b48b8cf95c703e29
Matt Newton <newton10471@gmail.com>
2025-04-02 11:58:22 -0400

    PING / PONG but not working

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,37 @@
-import * as net from "node:net";
+import net from 'net';
+import { Parser } from './parser.ts';
+import { Serializer } from './serializer.ts';
 
-const server = net.createServer((c) => {
-  // 'connection' listener.
-  console.log('client connected');
-  c.on('end', () => {
-    console.log('client disconnected');
-  });
-  c.write('hello\r\n');
-  c.pipe(c);
+// Create a TCP server
+const server = net.createServer((socket) => {
+	// Instantiate the parser for this connection.
+	const parser = new Parser();
+
+	// Listen for complete commands parsed from incoming data.
+	parser.on('command', (command: string[]) => {
+		// Check that we received a command and that it is PING.
+		if (command.length > 0 && command[0].toUpperCase() === 'PING') {
+			// Respond with PONG using the Serializer.
+			const reply = Serializer.serialize("PONG");
+			socket.write(reply);
+		} else {
+			// For any unsupported command, send an error reply.
+			const reply = Serializer.serialize(new Error("ERR unknown command"));
+			socket.write(reply);
+		}
+	});
+
+	// When data comes in from the client, feed it to the parser.
+	socket.on('data', (data: Buffer) => {
+		parser.feed(data);
+	});
+
+	socket.on('error', (err) => {
+		console.error('Socket error:', err);
+	});
 });
 
-server.on('error', (err) => {
-  throw err;
-});
-
+// Start listening on the standard Redis port.
 server.listen(6379, () => {
-  console.log('server bound');
+	console.log('Redis clone server listening on port 6379');
 });
